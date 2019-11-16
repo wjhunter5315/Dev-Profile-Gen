33562972f00d0951659785cb14c663418f258840
wjhunter5315 <wjhunter5315@gmail.com>
2019-11-15 18:16:18 -0800

    got all the info from github api, cant make pdf

--- index.js
+++ index.js
@@ -1,76 +1,99 @@
 const fs = require("fs");
-// const axios = require("axios");
-// const inquirer = require("inquirer");
+const axios = require("axios");
+const inquirer = require("inquirer");
+// const { app, BrowserWindow } = require('electron');
 
-// inquirer
-//   .prompt({
-//     message: "Enter your GitHub username:",
-//     name: "username"
-//   })
-//   .then(function({ username }) {
-//     const queryUrl = `https://api.github.com/users/:${username}`;
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}`;
 
-//     axios.get(queryUrl).then(function(response) {
-//         console.log(response);
-//     //   const repoNames = res.data.map(function(repo) {
-//     //     return repo.name;
-//       });
+    axios.get(queryUrl).then(function(response) {
+      console.log(response);
+      const userName = response.data.login;
+      console.log(userName);
+      const userFollowers = response.data.followers;
+      console.log(userFollowers);
+      const userFollowing = response.data.following;
+      console.log(userFollowing);
+      const userHome = response.data.html_url;
+      console.log(userHome);
+      const userRepos = response.data.public_repos;
+      console.log(userRepos);
+      // const repoNames = response.data.map(function(repo) {
+      //   return repo.name;
+      // });
 
-//     //   const repoNamesStr = repoNames.join("\n");
+      // const repoNamesStr = repoNames.join("\n");
 
-//     //   fs.writeFile("repos.txt", repoNamesStr, function(err) {
-//     //     if (err) {
-//     //       throw err;
-//     //     }
+      fs.writeFile("info.txt", userName, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+      fs.appendFile("info.txt", "\n"+userFollowers, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+      fs.appendFile("info.txt", "\n"+userFollowing, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+      fs.appendFile("info.txt", "\n"+userHome, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+      fs.appendFile("info.txt", "\n"+userRepos, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
 
-//     //     console.log(`Saved ${repoNames.length} repos`);
-//     //   });
-//     });
-// //   });
-const gitStarredURL = "https://api.github.com/users/wjhunter5315/starred";
-const githubURL = "https://api.github.com/users/wjhunter5315";
-$.ajax({
-  url: githubURL,
-  method: "GET"
-})
-.then(function(response) {
-  console.log(response);
-  const userName = response.login;
-  console.log(userName);
-  const userFollowers = response.followers;
-  console.log(userFollowers);
-  const userFollowing = response.following;
-  console.log(userFollowing);
-  const userHome = response.html_url;
-  console.log(userHome);
-  const userRepos = response.public_repos;
-  console.log(userRepos);
+      //   console.log(`Saved ${repoNames.length} repos`);
+    });
+    const gitStarredURL = `https://api.github.com/users/${username}/starred`
+    axios.get(gitStarredURL).then(function(response) {
+      console.log(response);
+      const starred = response.data.length;
+      console.log(starred);
+      fs.writeFile("starred.txt", starred, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+
+    
+  });
+
+  // function createWindow () {
+  //   // Create the browser window.
+  //   let win = new BrowserWindow({
+  //     width: 800,
+  //     height: 600,
+  //     webPreferences: {
+  //       nodeIntegration: true
+  //     }
+  //   })
   
-})
-const starred = [];
-$.ajax({
-  url: gitStarredURL,
-  method: "GET"
-})
-.then(function(response) {
-  console.log(response);
-  const starred = response.length;
-  console.log(starred);
+  //   // and load the index.html of the app.
+  //   win.loadFile('info.txt')
+  // }
   
-})
-fs.writeFile("info.txt", starred, function(err) {
-  if (err) {
-    throw err;
-  }
-})
-
-function getLocation() {
-  if (navigator.geolocation) {
-    console.log(navigator.geolocation);
-    // navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-getLocation();
-writeFile();
+  // app.on('ready', createWindow)
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     console.log(navigator.geolocation);
+//     // navigator.geolocation.getCurrentPosition(showPosition);
+//   } else { 
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
+// getLocation();
+// writeFile();
